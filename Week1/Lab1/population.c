211b7ec4ba706e447adf2ba16e1917737a0cd357
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-15 17:56:49 +0200

    File changes

---
+++ Week1/Lab1/population.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+}
+
+// Prompt starting population size
+//// If less than 9 then re-prompt
+
+// do prompt for n while llamas less than 9
+
+
+// Prompt for end population size
+
+
+// Calculate the number of years it takes
+//// Each year n / 3 llamas born
+//// Each year n / 4 llamas pass away
