70dbb20389ec3b93af84d6b95b9d8a7e730f434b
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-15 18:41:42 +0200

    Lab1 reg gekry!

--- Week1/Lab1/population.c
+++ Week1/Lab1/population.c
@@ -1,19 +1,49 @@
+#include <cs50.h>
 #include <stdio.h>
+
+int get_startPop(void);
+int get_endPop(void);
+void calculation(int n, int j);
 
 int main(void)
 {
-
+    int n = get_startPop();
+    int j = get_endPop();
+    calculation(n ,j);
 }
 
-// Prompt starting population size
-//// If less than 9 then re-prompt
 
-// do prompt for n while llamas less than 9
+// Prompt starting population size
+
+int get_startPop(void)
+{
+    int n;
+    do 
+    {
+        n = get_int("Starting Size: ");
+    }
+    while (n < 9);
+    return n;
+}
 
 
 // Prompt for end population size
 
+int get_endPop(void)
+{
+    int j = get_int("End Size: ");
+    return j;
+}
 
 // Calculate the number of years it takes
-//// Each year n / 3 llamas born
-//// Each year n / 4 llamas pass away
+
+void calculation(int n, int j)
+{
+    long y = 0;
+    for (int i = n; i <= j; i += (i/3 - i/4))
+    {
+        //printf("Years: %li \n", y += 1);
+        y += 1;
+    }
+    printf("Years: %li \n", y);
+}
