6ab7b475f216e46515fc683bd99297714384c816
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-19 23:55:57 +0200

    credit created and some pesudocode

---
+++ Week1/Pset1/credit.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+}
+
+//// printf kaart naam indien 3, 4, 5
+
+//// doen sommetjies met array
+
+//// as x / 10 = 0
+/// dan valide gee naam
