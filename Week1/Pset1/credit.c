5e4b6fc15a2150c4a933e6e3d7b616c8a792ed3d
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-20 21:44:34 +0200

    credit.c succesfully completed

--- Week1/Pset1/credit.c
+++ Week1/Pset1/credit.c
@@ -2,62 +2,88 @@
 #include <stdio.h>
 
 long number;
+long digits;
 
 int main(void)
 {
-    number = get_long("Choose a number! ");
+    number = get_long("Choose a number: ");
     printf("Number: %li \n", number);
+
+    long num = number;
+    int modA;
+    int modB;
+    int sumA = 0;
+    int sumB = 0;
+    int digA;
+    int digB;
+
+
+    long digits = 0;
+    for (long i = number; i > 0; i /= 10)
+    {
+        digits = digits + 1;
+    }
+    printf("Digits: %li \n", digits);
+
+
+    do
+    {
+        
+        modA = num % 10;
+        num = num / 10;
+        sumA = sumA + modA;
+
+        modB = num % 10;
+        num = num / 10;
+        
+        modB = modB * 2;
+       
+        if (modB > 9)
+        {
+            modB = 1 + (modB % 10);
+        }
+
+        sumB = sumB + modB;
+
+    } 
+    while (num > 0);
+    printf("SumA: %i \n", sumA);
+    printf("SumB: %i \n", sumB);
+    printf("ModA: %i \n", modA);
+    printf("ModB: %i \n", modB);
+
+
+    int total = sumA + sumB;
+    printf("Total: %i \n", total);
 
     if (number >= 340000000000000 && number < 350000000000000 || number >= 370000000000000 && number < 380000000000000)
     {
         printf("AMEX\n");
+        if (total % 10 != 0)
+        {
+            printf("INVALID\n");
+        }
     }
 
-    else if (number >= 5000000000000000 && number < 6000000000000000)
+    else if (number >= 5000000000000000 && number < 5600000000000000)
     {
         printf("MASTERCARD\n");
+        if (total % 10 != 0)
+        {
+            printf("INVALID\n");
+        }
     }
 
     else if (number >= 4000000000000000 && number < 5000000000000000 || number >= 4000000000000 && number < 5000000000000)
     {
         printf("VISA\n");
+        if (total % 10 != 0)
+        {
+            printf("INVALID\n");
+        }
     }
     else
     {
         printf("INVALID\n");
     }
 }
-
-// American express
-
-    // 15 digits
-    // begin met 34 of 37
-    // legit return =
-
-// Mastercard
-
-    // 16 digits
-    // begin met 51 - 55
-    // legit return = MASTERCARD\n
-
-// Visa
-
-    // 13 of 16 digits
-    // begin met 40 - 49
-    // legit return = VISA\n
-
-// if not valid
-
-    // return INVALID
-
-
-/*
-
-Take number
-
-    cut off last number for even numbers
-    cut off second to last number for odd numbers
-
-
-
-*/
