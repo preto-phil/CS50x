17fbd356a9613b753bae0b79caa1ab89e611e365
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-20 13:21:01 +0200

    credit progress

--- Week1/Pset1/credit.c
+++ Week1/Pset1/credit.c
@@ -1,14 +1,52 @@
 #include <cs50.h>
 #include <stdio.h>
 
+long number;
+
 int main(void)
 {
+    number = get_long("Choose a number! ");
+    printf("Number: %li \n", number);
 
+    // American express
+    if (number == isdigit())
+    {
+        if (number >= 340000000000000 && number < 350000000000000 || number >= 370000000000000 && number < 380000000000000)
+        {
+            printf("AMEX\n");
+        }
+
+        if (number >= 5000000000000000 && number < 60000000000000)
+        {
+            printf("MASTERCARD\n");
+        }
+
+        if (number >= 4000000000000000 && number < 50000000000000 || number >= 4000000000000 && number < 50000000000)
+        {
+            printf("VISA\n");
+        }
+    }
+    printf("Invalid \n");
 }
 
-//// printf kaart naam indien 3, 4, 5
+// American express
 
-//// doen sommetjies met array
+    // 15 digits
+    // begin met 34 of 37
+    // legit return = 
 
-//// as x / 10 = 0
-/// dan valide gee naam
+// Mastercard
+
+    // 16 digits
+    // begin met 51 - 55
+    // legit return = MASTERCARD\n
+
+// Visa
+
+    // 13 of 16 digits
+    // begin met 40 - 49
+    // legit return = VISA\n
+
+// if not valid
+
+    // return INVALID
