76d1b5e6cfd18d8fcba11119f891bfc9ed3aab20
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-20 13:56:28 +0200

    Credit prgoress: So te se klaar, moet net nog Luhn's Algorithm skryf en toepas

--- Week1/Pset1/credit.c
+++ Week1/Pset1/credit.c
@@ -8,32 +8,31 @@ int main(void)
     number = get_long("Choose a number! ");
     printf("Number: %li \n", number);
 
-    // American express
-    if (number == isdigit())
+    if (number >= 340000000000000 && number < 350000000000000 || number >= 370000000000000 && number < 380000000000000)
     {
-        if (number >= 340000000000000 && number < 350000000000000 || number >= 370000000000000 && number < 380000000000000)
-        {
-            printf("AMEX\n");
-        }
-
-        if (number >= 5000000000000000 && number < 60000000000000)
-        {
-            printf("MASTERCARD\n");
-        }
-
-        if (number >= 4000000000000000 && number < 50000000000000 || number >= 4000000000000 && number < 50000000000)
-        {
-            printf("VISA\n");
-        }
+        printf("AMEX\n");
     }
-    printf("Invalid \n");
+
+    else if (number >= 5000000000000000 && number < 6000000000000000)
+    {
+        printf("MASTERCARD\n");
+    }
+
+    else if (number >= 4000000000000000 && number < 5000000000000000 || number >= 4000000000000 && number < 5000000000000)
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 }
 
 // American express
 
     // 15 digits
     // begin met 34 of 37
-    // legit return = 
+    // legit return =
 
 // Mastercard
 
@@ -50,3 +49,15 @@ int main(void)
 // if not valid
 
     // return INVALID
+
+
+/*
+
+Take number
+
+    cut off last number for even numbers
+    cut off second to last number for odd numbers
+
+
+
+*/
